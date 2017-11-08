d158ff248b1f988de08ca80cb28d338132ed5c55
boldi24 <boldizsartompe@gmail.com>
2017-11-08 20:08:28 +0100

    Add dummy solution v0

--- javaloxon/src/ClientEndpoint.java
+++ javaloxon/src/ClientEndpoint.java
@@ -7,7 +7,7 @@ import javax.websocket.Session;
 
 public class ClientEndpoint extends Endpoint implements MessageHandler.Whole<String> {
 	private Session session;
-
+	private boolean isFirst = true;
 	@Override
 	public void onOpen(Session session, EndpointConfig config) {
 		session.addMessageHandler(this);
@@ -17,6 +17,13 @@ public class ClientEndpoint extends Endpoint implements MessageHandler.Whole<Str
 	@Override
 	public void onMessage(String message) {
 		System.out.println(message);
+		if(isFirst) {
+			isFirst = !isFirst;
+			sendMessage("{\"moveFrom\":102,\"moveTo\":106,\"armySize\":12}");
+			sendMessage("{\"moveFrom\":102,\"moveTo\":104,\"armySize\":11}");
+			sendMessage("{\"moveFrom\":102,\"moveTo\":108,\"armySize\":14}");
+			sendMessage("{\"moveFrom\":102,\"moveTo\":110,\"armySize\":13}");
+		}
 	}
 
 	private void sendMessage(String message) {
