98d8943edeaf9474c394e26363960d2c529663a1
Daniel Laszlo <daniel.laszlo94@gmail.com>
2017-11-15 17:44:12 +0100

    Normalizegit add auralux/src/main/java/com/loxon/javachallenge2017/pack/utility/Util.java !

---
+++ auralux/src/main/java/com/loxon/javachallenge2017/pack/utility/Util.java
@@ -0,0 +1,32 @@
+package com.loxon.javachallenge2017.pack.utility;
+
+import com.sun.java.swing.plaf.windows.WindowsTreeUI;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Util {
+    public static Map<Integer, Double> normalizeValues(Map<Integer, Double> values) {
+        Double min;
+        Double max;
+
+        min = values.entrySet().stream()
+                .mapToDouble(entry -> entry.getValue())
+                .min()
+                .getAsDouble();
+
+        max = values.entrySet().stream()
+                .mapToDouble(entry -> entry.getValue())
+                .max()
+                .getAsDouble();
+
+        return values.entrySet().stream()
+                .collect(
+                        Collectors.toMap(
+                                entry -> entry.getKey(),
+                                entry -> (entry.getValue() - min) / (max-min)
+                        )
+                );
+    }
+}
